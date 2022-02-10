fc2223e8a3ede455552da42c65d4b7a1bd2bf67a
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-10 17:53:16 +0530

    javascript code for click function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-let ham = document.querySelector('.ham');
-alert(ham);
+const ham=document.querySelectorAll('.hamburger');
+const artical=document.querySelectorAll('.amazing-artical');
+ham[0].addEventListener('click',function(){
+//    artical[0].style.transform='rotate(-90deg)';
+});
 
 
 
