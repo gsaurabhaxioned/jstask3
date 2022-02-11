df1ffd80632daee5b2be20a5e6c01daee6564573
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-11 08:51:49 +0530

    javascript for hamburger in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
+const main=document.getElementsByTagName('main');
 const ham=document.querySelectorAll('.hamburger');
 const right=document.querySelectorAll('.right');
 const menus=document.querySelectorAll('.menus');
 const cross=document.querySelectorAll('.cross');
-// const menus=document.querySelectorAll('.hamburger');
 ham[0].addEventListener('click',function(){
-   right[0].style.transform='rotate(-10deg)';
+   main[0].style.backgroundColor="#000";
+   right[0].style.transform='rotate(-30deg)';
    menus[0].style.transform='translate(100px)';
-   this.style.transform="rotate(-90deg)";
+   this.style.transform="rotate(90deg)";
    cross[0].style.transform='rotate(-90deg)';
    this.style.top='-20%';
    cross[0].style.left='12%';
