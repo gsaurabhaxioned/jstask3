00c563bd11d716d44af234df5e26c1f5784a6083
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-12 12:27:56 +0530

    js for rotation added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,29 @@
-// const main = document.getElementsByTagName('main');
-// const ham = document.querySelectorAll('.hamburger');
-// const right = document.querySelectorAll('.right');
-// const menus = document.querySelectorAll('.menus');
-// const cross = document.querySelectorAll('.cross');
-// ham[0].addEventListener('click', function () {
-//   this.classList.toggle('cross');
-//   right[0].classList.toggle('right-after-click');
-//   main[0].classList.toggle('main-after-click');
-//    // main[0].style.backgroundColor = "#000";
-//    // right[0].style.transform = 'rotate(-20deg)';
-//    // right[0].style.transformOrigin = '0 0';
-//    // menus[0].style.left = "10%";
-//    // menus[0].style.paddingTop = "35%";
-//    // this.classList.add('cross');
-//    // this.classList.remove('hamburger');
+const main_page = document.querySelectorAll('.main-page');
+const hamburger_controls= document.querySelectorAll('.hamburger');
+const ham = document.querySelectorAll('.ham');
+const cross = document.querySelectorAll('.cross');
+ham[0].addEventListener('click', function () {
+  hamburger_controls[0].style.transform="rotate(-90deg)";
+  main_page[0].style.transform="rotate(-20deg)";
+  });
+
+cross[0].addEventListener('click',function(){
+  hamburger_controls[0].style.transform="rotate(0)";
+  main_page[0].style.transform="rotate(0deg)";
+})
+
+  // this.classList.toggle('cross');
+  // right[0].classList.toggle('right-after-click');
+  // main[0].classList.toggle('main-after-click');
+   // main[0].style.backgroundColor = "#000";
+   // right[0].style.transform = 'rotate(-20deg)';
+   // right[0].style.transformOrigin = '0 0';
+   // menus[0].style.left = "10%";
+   // menus[0].style.paddingTop = "35%";
+   // this.classList.add('cross');
+   // this.classList.remove('hamburger');
    
-//    // console.log(cross);
-//    // console.log(cross[0]);
-// });
+   // console.log(cross);
+   // console.log(cross[0]);
+
 
