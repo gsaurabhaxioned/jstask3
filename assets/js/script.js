012bfa5488023bbc69658ce3b24d5db73d3efe9d
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-12 10:33:07 +0530

    js code written for class toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,20 @@
-const main=document.getElementsByTagName('main');
-const ham=document.querySelectorAll('.hamburger');
-const right=document.querySelectorAll('.right');
-const menus=document.querySelectorAll('.menus');
-const cross=document.querySelectorAll('.cross');
-ham[0].addEventListener('click',function(){
-   main[0].style.backgroundColor="#000";
-   right[0].style.transform='rotate(-30deg)';
-   right[0].style.transform='rotate(-30deg)';
-   right[0].style.transformOrigin='0 0';
-   this.classList.remove('hamburger');
-   this.classList.add('cross');
-  });
+const main = document.getElementsByTagName('main');
+const ham = document.querySelectorAll('.hamburger');
+const right = document.querySelectorAll('.right');
+const menus = document.querySelectorAll('.menus');
+const cross = document.querySelectorAll('.cross');
+ham[0].addEventListener('click', function () {
 
-cross[0].addEventListener('click',function(){
-   main[0].style.backgroundColor="#fff";
-   right[0].style.transform='rotate(0)';
-   menus[0].style.transform='translate(0)';
-   this.classList.remove('cross');
-   this.classList.add('hamburger');
+  this.classList.toggle('cross');
+   // main[0].style.backgroundColor = "#000";
+   // right[0].style.transform = 'rotate(-20deg)';
+   // right[0].style.transformOrigin = '0 0';
+   // menus[0].style.left = "10%";
+   // menus[0].style.paddingTop = "35%";
+   // this.classList.add('cross');
+   // this.classList.remove('hamburger');
+   
+   // console.log(cross);
+   // console.log(cross[0]);
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
